e0399770e87e701677a59ac2325d0b8da3900ccc
Kikuo Emoto <kikuomax@gmail.com>
2020-07-02 23:28:52 +0900

    feat: initial build script
    - `webpack.config.js` builds `dist/main.js` from `src/index.js`.
    - `.gitignore` includes `/dist`.

---
+++ src/index.js
@@ -0,0 +1,3 @@
+/**
+ * An entry point.
+ */
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+const defaultMode = 'development'
+
+module.exports = {
+  mode: defaultMode,
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
