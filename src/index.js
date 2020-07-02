c7959bf60e57aa123baaacff13731fc62a7b42ff
Kikuo Emoto <kikuomax@gmail.com>
2020-07-03 00:19:25 +0900

    feat: render default mapbox map
    - `src/index.ejs` renders a default mapbox map.
        - Mapbox GL JS is introduced.
        - `src/index.js` loads the map behind the scene.
    - `webpack.config.js` supports loading css files.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 /**
  * An entry point.
  */
+
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const urlParams = new URLSearchParams(window.location.search)
+const accessToken = urlParams.get('access_token')
+mapboxgl.accessToken = accessToken
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-74.5, 40],
+  zoom: 9
+})
