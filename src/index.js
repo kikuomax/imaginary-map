c816e4ddedaf2dd859185630c39bafc66280c5ee
Kikuo Emoto <kikuomax@gmail.com>
2020-07-04 18:53:16 +0900

    feat: use custom style
    - `src/index.js` uses a custom style which only contains 'background'
      and 'imaginary' layers; i.e., no dependency on the Mapbox server.

--- src/index.js
+++ src/index.js
@@ -6,38 +6,66 @@ import mapboxgl from 'mapbox-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-console.log(window.location.origin)
+// gets directory name from a given path.
+function getDirname (path) {
+  if (path.endsWith('/')) {
+    return path
+  } else {
+    const slashIndex = path.lastIndexOf('/')
+    if (slashIndex !== -1) {
+      return path.substring(0, slashIndex + 1)
+    } else {
+      return path
+    }
+  }
+}
+
+const dirPath = getDirname(window.location.pathname)
+const baseUrl = `${window.location.origin}${dirPath}`
+if (process.env.NODE_ENV !== 'production') {
+  console.log('baseUrl', baseUrl)
+}
 const urlParams = new URLSearchParams(window.location.search)
 const accessToken = urlParams.get('access_token')
 mapboxgl.accessToken = accessToken
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
+  style: {
+    version: 8,
+    name: "imaginary",
+    sources: {
+      imaginary: {
+        type: 'vector',
+        tiles: [
+          `${baseUrl}tiles/{z}/{x}/{y}.pbf`
+        ],
+        minzoom: 0,
+        maxzoom: 0
+      }
+    },
+    layers: [
+      {
+        id: 'background',
+        type: 'background',
+        paint: {
+          'background-color': '#505050'
+        }
+      },
+      {
+        id: 'imaginary',
+        type: 'fill',
+        source: 'imaginary',
+        'source-layer': 'test',
+        paint: {
+          'fill-color': '#AFDB1C',
+          'fill-outline-color': '#181E04'
+        }
+      }
+    ]
+  },
   center: [-74.5, 40],
   zoom: 9,
   // renderWorldCopies:false makes only one map rendered
   // https://github.com/mapbox/mapbox-gl-js/pull/3885
   renderWorldCopies: false
-})
-map.on('load', () => {
-  map.addSource('imaginary', {
-    type: 'vector',
-    tiles: [
-      `${window.location.origin}/tiles/{z}/{x}/{y}.pbf`
-    ],
-    minzoom: 0,
-    maxzoom: 0
-  })
-  map.addLayer(
-    {
-      id: 'imaginary',
-      type: 'fill',
-      source: 'imaginary',
-      'source-layer': 'test',
-      paint: {
-        'fill-color': '#AFDB1C',
-        'fill-outline-color': '#181E04'
-      }
-    }
-  )
 })
