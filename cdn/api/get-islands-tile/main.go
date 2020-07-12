f1224b01941aaeb3f116d6df6a8ea1aae2ef1bde
Kikuo Emoto <kikuomax@gmail.com>
2020-07-13 01:57:17 +0900

    feat(cdn): load islands GeoJSON
    - The `get-islands-tile` function loads a GeoJSON of islands.
      It returns the contents of the GeoJSON file istead of a map vector
      tile so far.

--- cdn/api/get-islands-tile/main.go
+++ cdn/api/get-islands-tile/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	runtime "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
+
+// Environment variable names.
+const GEO_JSON_BUCKET_NAME = "GEO_JSON_BUCKET_NAME"
+const ISLANDS_GEO_JSON_VERSION = "ISLANDS_GEO_JSON_VERSION"
+
+// GeoJSON file name.
+const GEO_JSON_FILE_NAME = "islands.json"
 
 // Common form of a GetTileEvent.
 type GetTileEvent struct {
@@ -12,8 +27,44 @@ type GetTileEvent struct {
 	Y int `json:"y"`
 }
 
+// Loads the GeoJSON for islands.
+func LoadGeoJson () ([]byte, error) {
+	bucketName, ok := os.LookupEnv(GEO_JSON_BUCKET_NAME)
+	if !ok {
+		return nil, errors.New(
+			fmt.Sprintf(
+				"environment variable %s is not set",
+				GEO_JSON_BUCKET_NAME))
+	}
+	version, ok := os.LookupEnv(ISLANDS_GEO_JSON_VERSION)
+	if !ok {
+		return nil, errors.New(
+			fmt.Sprintf(
+				"environment variable %s is not set",
+				ISLANDS_GEO_JSON_VERSION))
+	}
+	key := fmt.Sprintf("/%s/%s", version, GEO_JSON_FILE_NAME)
+	objectIn := s3.GetObjectInput {
+		Bucket: aws.String(bucketName),
+		Key: aws.String(key),
+	}
+	log.Println("loading GeoJSON", bucketName, key)
+	client := s3.New(session.New())
+	objectOut, err := client.GetObject(&objectIn)
+	if err != nil {
+		return nil, err
+	}
+	bodyIn := objectOut.Body
+	defer bodyIn.Close()
+	body, err := ioutil.ReadAll(bodyIn)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
 func HandleRequest (ctx context.Context, event GetTileEvent) ([]byte, error) {
-	return []byte{ 1, 2, 3 }, nil
+	return LoadGeoJson()
 }
 
 func main () {
